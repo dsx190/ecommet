30a5fe9747be527a3a36c7d89829a867d6376b6c
David Salas <david.salas@jonajo.com>
2017-01-21 23:44:12 -0600

    Unit tests for DB

---
+++ tests/unit/core.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Db = require('../../lib/models/core/db'),
+	expect = require('chai').expect;
+
+describe('Db', () => {
+
+	it('should connect to the Database', (done) => {
+		Db.connect().then(db => {
+			expect(db).to.be.an('object');
+			db.close();
+			done();
+		});
+	});
+
+	it('should insert one doc', (done) => {
+		Db.insertOne('tests', {'dummy': 'test'}).then(res => {
+			expect(res.insertedCount).to.equal(1);
+			done();
+		});
+	});
+
+	it('should insert many docs', (done) => {
+		Db.insertMany('tests', [
+			{'one': '1'},
+			{'two': '2'}
+		]).then(res => {
+			expect(res.insertedCount).to.equal(2);
+			done();
+		});
+	});
+
+	it('should find one doc', (done) => {
+		Db.findOne('tests', {'dummy': 'test'}).then(doc => {
+			expect(doc).to.be.an('object');
+			expect(doc.dummy).to.equal('test');
+			done();
+		});
+	});
+
+	it('should find docs', (done) => {
+		Db.find('tests', {'dummy': 'test'}).then(docs => {
+			expect(docs[0]).to.be.an('object');
+			expect(docs[0].dummy).to.equal('test');
+			done();
+		});
+	});
+
+	it('should delete one doc', (done) => {
+		Db.deleteOne('tests', {'dummy': 'test'}).then(res => {
+			expect(res.deletedCount).to.equal(1);
+			done();
+		});
+	});
+
+	it('should delete many docs', (done) => {
+		Db.deleteMany('tests', {}).then(res => {
+			expect(res.result.ok).to.equal(1);
+			done();
+		});
+	});
+});
