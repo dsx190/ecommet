43cfbd9fae8d2c16d0b60bedc3411486cf0f3ef0
David Salas <david.salas@jonajo.com>
2017-01-28 02:42:33 -0600

    Progress on customers and customer addresses

--- routes/admin/customer.js
+++ routes/admin/customer.js
@@ -9,5 +9,8 @@ router.get('/create', controller.create);
 router.get('/edit/:id?', controller.edit);
 router.post('/save/:id?', controller.save);
 router.get('/delete/:id', controller.delete);
+router.get('/:customerId/address/delete/:id', controller.deleteAddress);
+router.get('/:customerId/address/:id?', controller.editAddress);
+router.post('/:customerId/address/:id?', controller.saveAddress);
 
 module.exports = router;
