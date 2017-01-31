9409d0aab96e7055a28c6648e8d3b8d5c1cb9bd6
David Salas <david.salas@jonajo.com>
2017-01-31 01:06:43 -0600

    Attribute sets

--- routes/admin/catalog.js
+++ routes/admin/catalog.js
@@ -3,16 +3,16 @@
 const express = require('express'),
 	router = express.Router(),
 	attributeCtrl = require('../../lib/controllers/admin/attribute'),
-	attributeSetCtrl = require('../../lib/controllers/admin/attributeSet');
+	attributeSetCtrl = require('../../lib/controllers/admin/attribute-set');
 
 router.get('/attributes/', attributeCtrl.index);
 router.get('/attributes/edit/:id?', attributeCtrl.edit);
 router.post('/attributes/save/:id?', attributeCtrl.save);
 router.get('/attributes/delete/:id', attributeCtrl.delete);
 
-/*router.get('/attributeSets/', attributeSetCtrl.index);
+router.get('/attributeSets/', attributeSetCtrl.index);
 router.get('/attributeSets/edit/:id?', attributeSetCtrl.edit);
 router.post('/attributeSets/save/:id?', attributeSetCtrl.save);
-router.get('/attributeSets/delete/:id', attributeSetCtrl.delete);*/
+router.get('/attributeSets/delete/:id', attributeSetCtrl.delete);
 
 module.exports = router;
