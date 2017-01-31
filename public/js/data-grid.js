5daee280c58ff72728a456161c05852032d038ef
David Salas <david.salas@jonajo.com>
2017-01-30 22:53:17 -0600

    Using short sintax of jQuery

--- public/js/data-grid.js
+++ public/js/data-grid.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(() => {
+jQuery(document).ready(($) => {
 	var url = baseUrl,
 		sorted, sortBy, sortDir,
 		appendSorts = (by, dir) => { // Shortcut
@@ -13,8 +13,8 @@ jQuery(document).ready(() => {
 			url += `?p=${page}`;
 			
 			// Search filters
-			jQuery('table thead th input, table thead th select').each((index, el) => {
-				el = jQuery(el);
+			$('table thead th input, table thead th select').each((index, el) => {
+				el = $(el);
 				if (el.val()) {
 					url += `&${el.data('filter')}=${el.val()}`;
 				}
@@ -24,8 +24,8 @@ jQuery(document).ready(() => {
 			if (sorted && sortDir) {
 				appendSorts(sortBy, sortDir);
 			} else if (!sorted) {
-				jQuery('table thead th').each((index, el) => {
-					el = jQuery(el);
+				$('table thead th').each((index, el) => {
+					el = $(el);
 					if (el.data('sort-dir')) {
 						appendSorts(el.data('sort-by'), el.data('sort-dir'));
 					}
@@ -36,10 +36,10 @@ jQuery(document).ready(() => {
 	/**
 	 * Update sort data and call search when clicking on a table header.
 	 */
-	jQuery('table thead th').on('click', e => {
+	$('table thead th').on('click', e => {
 		// Exit when clicking on input or select.
 		if (e.target.tagName === 'LABEL') {
-			let el = jQuery(e.currentTarget),
+			let el = $(e.currentTarget),
 				prevDir = el.data('sort-dir');
 			sortBy = el.data('sort-by');
 			if (prevDir && prevDir == 1) {
@@ -55,35 +55,35 @@ jQuery(document).ready(() => {
 	 * Execute search when changing select values,
 	 * clicking the search button or pressing enter on input fields.
 	 */
-	jQuery('table thead th select').on('change', search);
-	jQuery('button[data-action="search"]').on('click',search);
-	jQuery('table thead th input').on('keypress', e => {
+	$('table thead th select').on('change', search);
+	$('button[data-action="search"]').on('click',search);
+	$('table thead th input').on('keypress', e => {
 		if (e.keyCode == 13) search();
 	});
 	/**
 	 * Reset filters when clicking on the reset button.
 	 */
-	jQuery('button[data-action="reset"]').on('click', () => {
+	$('button[data-action="reset"]').on('click', () => {
 		window.location.href = url;
 	});
 	/**
 	 * Change the page number when clicking on the page controls or 
 	 * pressing enter on the text input.
 	 */
-	jQuery('button[data-action="page"').on('click', e => {
-		page = jQuery(e.currentTarget).data('next')? page + 1 : page - 1;
+	$('button[data-action="page"').on('click', e => {
+		page = $(e.currentTarget).data('next')? page + 1 : page - 1;
 		search();
 	});
-	jQuery('input[data-action="page"]').on('keypress', e => {
+	$('input[data-action="page"]').on('keypress', e => {
 		if (e.keyCode === 13) {
-			page = jQuery(e.currentTarget).val();
+			page = $(e.currentTarget).val();
 			search();
 		}
 	});
 	/**
 	 * Go to the edit URL when clicking on a row on the grid.
 	 */
-	jQuery('table tbody tr[data-action="edit"]').on('click', e => {
+	$('table tbody tr[data-action="edit"]').on('click', e => {
 		url = jQuery(e.currentTarget).data('url');
 		window.location.href = url;
 	});
